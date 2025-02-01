08946826a00be9a5ede2f5fcac31b2feaf07a919
jaythree <giljr.2009@gmail.com>
2025-02-01 15:41:31 -0400

    💅 Third Episode (devise)

--- Gemfile
+++ Gemfile
@@ -67,3 +67,5 @@ group :test do
 end
 
 gem "tailwindcss-rails", "~> 3.3"
+
+gem "devise", "~> 4.9"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :rooms
+  devise_for :users
   root 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
